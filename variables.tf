9a9248d9c6b6bf0afe7db5b01d4dd89b7d6b3af9
Maire Kehoe <mkehoe@ie.ibm.com>
2022-10-04 09:52:32 +0100

    cluster_namespace to prod-ns-b

--- variables.tf
+++ variables.tf
@@ -54,7 +54,7 @@ variable "cluster_name" {
 variable "cluster_namespace" {
   type        = string
   description = "Name of the kubernetes cluster where the application will be deployed."
-  default     = "prod-ns-a"
+  default     = "prod-ns-b"
 }
 
 variable "cluster_region" {
